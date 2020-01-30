5d20574e79dcba53d0ca9deff0114cfafe44bd93
某亚瑟 <i@yojigen.tech>
2020-01-30 15:59:45 +0800

    WiP:update some config update commands update static files config update mysql config

--- django_cli/settings/prod.py
+++ django_cli/settings/prod.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
+    }
+}
